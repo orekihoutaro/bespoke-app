baf448f150f93fcbcd6f9906c6df26277aca0a4e
bespoke-it-admin2023 <jomer.bespoke@gmail.com>
2023-02-10 15:52:26 +0800

    View Adjustment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,26 +2,31 @@ import React from 'react'
 import logo from '../assets/logo.png'
 import styles from '../style'
 import fb_logo from '../assets/fb_logo.png'
-
-
-
+import linkedin_logo from '../assets/linkedin_logo.png'
 
 const Footer = () =>  {
     const handleClick = () => {
         window.open("www.facebook.com/MyBespokeStaff", "_blank")
     }
     return(
-        <div className={`${styles.paddingY}`}>
-            <div className={` ${styles.boxWidth} pl-8`}>
+        <div className={`p-1`}>
+            <div className={` ${styles.boxWidth} px-6`}>
                 <a href='/home'>
                     <img src={ logo } alt="logo" className='w-[145px] h-[145px] cursor-pointer'/>
                 </a>
             </div>
-        <div className={`${styles.boxWidth} pl-10 pt-4`}>
-            <button onClick={handleClick}>
-                <img src={ fb_logo } alt="fb_logo" className="w-[32px] h-[32px]"/>
-            </button>
-        </div>
+            <div className={`${styles.boxWidth} grid grid-flow-col w-1/4`}>
+                <div className={`${styles.boxWidth} pl-6 pt-4`}>
+                    <button onClick={handleClick}>
+                        <img src={ fb_logo } alt="fb_logo" className="w-[32px] h-[32px]"/>
+                    </button>
+                </div>
+                <div className={`${styles.boxWidth} pl-6 pt-4`}>
+                    <button onClick={handleClick}>
+                        <img src={ fb_logo } alt="fb_logo" className="w-[32px] h-[32px]"/>
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,8 @@ import styles from '../style'
 
 const Hero = () => (
   <section id="home" className={`md:flex-row flex-col w-full`}>
-      <div className="w-full h-96 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white font-bold px-6">
-        <div className={`flex flex-row justify-between items-center`}>
+      <div className="w-full h-96 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white font-bold py-8 px-8">
+        <div className={`flex flex-row justify-between items-center py-4 px-6`}>
           <h1 className="flex-1 font-poppins font-semibold ss:text-[55px] text-[12px] text-white ss:leading-[100px] leading-[75px]">
             WE TAKE THE {" "}<br className="sm:block hidden"/>
             <span className="text-yellow-400">COMPICATED</span> {" "} OUT OF <br />
